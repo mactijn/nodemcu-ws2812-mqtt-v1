d270994ac83b97913f29387b6a52fcc1a2b0d6fd
Martijn Bakker <martijn@wetransfer.com>
2016-03-05 16:15:46 +0100

    3 leds

--- rainbow.lua
+++ rainbow.lua
@@ -2,7 +2,6 @@
 
 -- source: http://www.krishnamani.in/color-codes-for-rainbow-vibgyor-colours/
 -- adjusted for grb
-off = string.char(0, 0, 0)
 red = string.char(0, 255, 0)
 orange = string.char(127, 255, 0)
 yellow = string.char(255, 255, 0)
@@ -10,21 +9,22 @@ green = string.char(255, 0, 0)
 blue = string.char(0, 0, 255)
 indigo = string.char(0, 75, 130)
 violet = string.char(0, 148, 211)
-on = string.char(255, 255, 255)
+white = string.char(255, 255, 255)
+black = string.char(0, 0, 0)
 
 
 interval = 100
 patterns = {
-    red .. orange .. yellow .. green,
-    orange .. yellow .. green .. blue,
-    yellow .. green .. blue .. indigo,
-    green .. blue .. indigo .. violet,
-    blue .. indigo .. violet .. red,
-    indigo .. violet .. red .. orange,
-    violet .. red .. orange .. yellow,
+    red .. orange .. yellow,
+    orange .. yellow .. green,
+    yellow .. green .. blue,
+    green .. blue .. indigo,
+    blue .. indigo .. violet,
+    indigo .. violet .. red,
+    violet .. red .. orange,
 }
 
-leds = ws2812.newBuffer(4)
+leds = ws2812.newBuffer(3)
 leds:fill(0, 0, 0)
 id = 0
 tmr.alarm(0, interval, tmr.ALARM_SEMI, function()
